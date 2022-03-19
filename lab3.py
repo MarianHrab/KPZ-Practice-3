7324c42e77f4f3be054d35588e980668de6ee501
MarianHrab <99960652+MarianHrab@users.noreply.github.com>
2022-03-19 14:18:03 +0300

    lab3

--- lab3.py
+++ lab3.py
@@ -1,24 +1,26 @@
+import ipaddress
+import platform
 
 
 def palindrom(text):
 
-    #put the words from imput to word list
+    # put the words from input to word list
 
     word_list =[]
     temp = ''
     for character in text:
-        if character.isalpha(): #checking is it every character is from alphabet
-            temp += character #add to the temporary variable
-        else:  #if it's not the character is alphabet, add our word to wordlist, and clear the temp variable
+        if character.isalpha(): # checking is it every character is from alphabet
+            temp += character # add to the temporary variable
+        else:  # if it's not the character is alphabet, add our word to wordlist, and clear the temp variable
             word_list.append(temp)
             temp = ''
 
-    #checking is the word is palinrom
+    # checking is the word is palindrom
 
     pal_words = []
     for i in word_list:
-        reversed_word = i[::-1] #making words reversed
-        if reversed_word == i: #checking is it read same as the original word,if yes add to pal_words
+        reversed_word = i[::-1]  # making words reversed
+        if reversed_word == i:  # checking is it read same as the original word,if yes add to pal_words
             pal_words.append(i)
     return pal_words
 
@@ -26,4 +28,23 @@ def palindrom(text):
 palindrom("abc addd ababa qwertyytrewq ada ")
 
 
+def validate_ip(ip_address):
+    ip_validate = False
+    try:
+        ip = ipaddress.ip_address(ip_address)
+        ip_validate = True
+    except ValueError:
+        ip_validate = False
 
+    return ip_validate
+
+
+validate_ip("192.168.1.42")
+validate_ip("192.168.1.424")
+
+
+def get_os():
+    return print(platform.system())
+
+
+get_os()
