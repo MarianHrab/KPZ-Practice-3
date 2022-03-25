c09a43067f81c36374fcddcb5b08b1d0f3f22b9a
MarianHrab <99960652+MarianHrab@users.noreply.github.com>
2022-03-25 12:40:36 +0300

    lab3

--- lab3.py
+++ lab3.py
@@ -2,15 +2,42 @@ import ipaddress
 import platform
 
 
-def palindrom(text):
+# def palindrom(text):
+#
+#     # put the words from input to word list
+#
+#     word_list =[]
+#     temp = ''
+#     for character in text:
+#         if character.isalpha():  # checking is it every character is from alphabet
+#             temp += character  # add to the temporary variable
+#         else:  # if it's not the character is alphabet, add our word to wordlist, and clear the temp variable
+#             word_list.append(temp)
+#             temp = ''
+#
+#
+#     # checking is the word is palindrom
+#
+#     pal_words = []
+#     for i in word_list:
+#         reversed_word = i[::-1]  # making words reversed
+#         if reversed_word == i:  # checking is it read same as the original word,if yes add to pal_words
+#             pal_words.append(i)
+#     return pal_words
+#
+#
+# palindrom("abc adda aba qwertyytrewq ada ")
 
+def palindrome(text):
     # put the words from input to word list
 
-    word_list =[]
+    word_list = []
     temp = ''
-    for character in text:
-        if character.isalpha():  # checking is it every character is from alphabet
-            temp += character  # add to the temporary variable
+    for i, v in enumerate(text):
+        if v.isalpha():  # checking is it every character is from alphabet
+            temp += v
+            if i == len(text) - 1 and temp.isalpha():
+                word_list.append(temp)  # add to the temporary variable
         else:  # if it's not the character is alphabet, add our word to wordlist, and clear the temp variable
             word_list.append(temp)
             temp = ''
@@ -20,13 +47,14 @@ def palindrom(text):
     pal_words = []
     for i in word_list:
         reversed_word = i[::-1]  # making words reversed
+        if len(i) < 3:
+            break
         if reversed_word == i:  # checking is it read same as the original word,if yes add to pal_words
             pal_words.append(i)
     return pal_words
 
 
-palindrom("abc addd ababa qwertyytrewq ada ")
-
+print(palindrome("abc ada abba a"))
 
 def validate_ip(ip_address):
     ip_validate = False
